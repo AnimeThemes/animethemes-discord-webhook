d25875ad0c9155867c943013f88027cf53c4f57b
Kyrch <kyrchk5@gmail.com>
2024-06-02 15:25:08 -0300

    fix: fetching thread instead of finding it (#17)

--- src/api/notification.ts
+++ src/api/notification.ts
@@ -1,17 +1,22 @@
 import { client, server } from 'app'
-import { Channel, ThreadChannel } from 'discord.js';
+import { Channel, ForumChannel, ThreadChannel } from 'discord.js';
 import { createVideoEmbedByAnime } from 'discord/embeds';
+
+import config from 'utils/config';
 
 export default () => {
     server.post('/notification', async (req, res) => {
         let body = req.body as any;
-        let threadId: any;
+        let threadId: string = '';
         let thread;
+
+        const forum = client.channels.cache.find((channel: Channel) => channel.id == config.DISCORD_FORUM_CHANNEL_ID) as ForumChannel;
+
         try {
             for (let video of body.videos) {
                 threadId = video.animethemeentries[0].animetheme.anime.discordthread.thread_id;
-                thread = client.channels.cache.find((channel: Channel) => channel.id == threadId) as ThreadChannel;
-    
+                thread = await forum.threads.fetch(threadId) as ThreadChannel;
+
                 thread.send({
                     embeds: [createVideoEmbedByAnime(video, body.type)]
                 });
